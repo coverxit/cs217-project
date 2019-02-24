f2d0fb1f7e44ea28b83c283e91377ff9c9228af4
Shindo Wu <cn.shindo@gmail.com>
2019-02-23 17:58:12 -0800

    Concurrent I/O

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <memory>
+#include <thread>
+#include <vector>
+
+#include <stdint.h>
+#include <string.h>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "ConcurrentFileReader.hpp"
+#include "ConcurrentFileWriter.hpp"
+
+int main(int argc, char const* argv[])
+{
+    return 0;
+}
