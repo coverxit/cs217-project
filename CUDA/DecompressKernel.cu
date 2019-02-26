0f5f4c460f18d33861199d8a2ba6a9094738c636
Shindo Wu <cn.shindo@gmail.com>
2019-02-26 03:53:15 -0800

    Fix

--- CUDA/DecompressKernel.cu
+++ CUDA/DecompressKernel.cu
@@ -14,8 +14,6 @@
 __global__ void DecompressKernel(CompressFlagBlock* deviceFlagIn, int nFlagBlocks,
     const uint8_t* deviceInBuf, uint8_t* deviceOutBuf)
 {
-    CompressFlagBlock flagBlock;
-
     // This is the data blockId. Each thread is responsible for a 4KB data block.
     auto blockId = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -25,7 +23,7 @@ __global__ void DecompressKernel(CompressFlagBlock* deviceFlagIn, int nFlagBlock
         auto outOffset = blockId * DataBlockSize;
 
         for (int j = 0; j < numOfFlags; ++j) {
-            if (GET_BIT(flagBlock.Flags, j) == 0) {
+            if (GET_BIT(deviceFlagIn[blockId].Flags, j) == 0) {
                 // Single character
                 deviceOutBuf[outOffset] = deviceInBuf[inOffset];
                 ++inOffset;
