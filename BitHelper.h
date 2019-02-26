5b1298e24ff6eb4089eb75f863c1593190feaed6
Shindo Wu <cn.shindo@gmail.com>
2019-02-25 16:00:48 -0800

    Refactor

---
+++ BitHelper.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define GET_BIT(flags, bit) \
+    (((flags)[(bit) / 8] >> ((bit) % 8)) & 0x1)
+
+#define PUT_BIT(flags, bit, value) \
+    PUT_BIT_##value(flags, bit)
+
+#define PUT_BIT_0(flags, bit) \
+    ((flags)[(bit) / 8] &= ~(1 << ((bit) % 8)))
+
+#define PUT_BIT_1(flags, bit) \
+    ((flags)[(bit) / 8] |= (1 << ((bit) % 8)))
+
+#define SIZE_OF_FLAGS(numFlags) \
+    ((numFlags) - 1) / 8 + 1
