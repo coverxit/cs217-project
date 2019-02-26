7b1f32547279503320a6ee648ba369701f2bd5a2
Shindo Wu <cn.shindo@gmail.com>
2019-02-26 12:42:14 -0800

    Optimize memory access

--- CUDA/DecompressKernel.cu
+++ CUDA/DecompressKernel.cu
@@ -14,26 +14,29 @@
 __global__ void DecompressKernel(CompressFlagBlock* deviceFlagIn, int nFlagBlocks,
     const uint8_t* deviceInBuf, uint8_t* deviceOutBuf)
 {
+    CompressFlagBlock flagBlock;
+    uint8_t inBuf[DataBlockSize], outBuf[DataBlockSize];
+
     // This is the data blockId. Each thread is responsible for a 4KB data block.
     auto blockId = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (blockId < nFlagBlocks) {
-        CompressFlagBlock flagBlock;
         memcpy(&flagBlock, deviceFlagIn + blockId, sizeof(CompressFlagBlock));
+        memcpy(inBuf, deviceInBuf + flagBlock.CompressedOffset, flagBlock.CompressedSize);
 
-        auto inOffset = flagBlock.CompressedOffset;
-        auto outOffset = blockId * DataBlockSize;
+        auto inOffset = 0;
+        auto outOffset = 0;
 
         for (int j = 0; j < flagBlock.NumOfFlags; ++j) {
             if (GET_BIT(flagBlock.Flags, j) == 0) {
                 // Single character
-                deviceOutBuf[outOffset] = deviceInBuf[inOffset];
+                outBuf[outOffset] = inBuf[inOffset];
                 ++inOffset;
                 ++outOffset;
             } else {
                 // Replacement pair
                 PairType matchPair;
-                memcpy(&matchPair, deviceInBuf + inOffset, sizeof(PairType));
+                memcpy(&matchPair, inBuf + inOffset, sizeof(PairType));
 
                 // Plus 1 for the opposite operation in compression
                 auto matchOffset = (matchPair >> PairLengthBits) + 1;
@@ -41,12 +44,15 @@ __global__ void DecompressKernel(CompressFlagBlock* deviceFlagIn, int nFlagBlock
 
                 // May overlap, so manually copy
                 for (int k = 0; k < matchLength; ++k) {
-                    deviceOutBuf[outOffset] = deviceOutBuf[outOffset - matchOffset];
+                    outBuf[outOffset] = outBuf[outOffset - matchOffset];
                     ++outOffset;
                 }
 
                 inOffset += 2;
             }
         }
+
+        // Copy back to global memory
+        memcpy(deviceOutBuf + blockId * DataBlockSize, outBuf, outOffset);
     }
 }
