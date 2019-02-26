f241ade60fbac522831172d7f4c5aea27560784f
Shindo Wu <cn.shindo@gmail.com>
2019-02-25 21:44:12 -0800

    Refactor

--- CUDA/CUDALZSS.cu
+++ CUDA/CUDALZSS.cu
@@ -15,10 +15,10 @@
 #include "CUDALZSS.h"
 
 #define cudaCheckError(op, msg) \
-    do { cudaError_t ret = (op); if ((ret) != cudaSuccess) _gerror((ret), (msg), __LINE__); } while (false)
+    do { cudaError_t ret = (op); if ((ret) != cudaSuccess) _gerror((ret), (msg)); } while (false)
 
-inline void _gerror(cudaError_t cudaError, const char* msg, int lineNo) {
-    fprintf(stderr, "[%s:%d] %s, cudaError = %d\n", lineNo, __FILE__, msg, cudaError);
+inline void _gerror(cudaError_t cudaError, const char* msg) {
+    fprintf(stderr, "%s, cudaError = %d\n", msg, cudaError);
     exit(-1);
 }
 
@@ -38,15 +38,16 @@ std::pair<bool, double> CUDALZSS::compress(const uint8_t* inBuf, int inSize,
     int *deviceOutSize, *deviceFlagSize;
 
     printf("Allocating device variables...\n");
-    cudaCheckError(cudaMalloc((void**) &deviceInBuf, inSize), "Failed to allocate device memory");
-    cudaCheckError(cudaMalloc((void**) &deviceOutBuf, outSize), "Failed to allocate device memory");
-    cudaCheckError(cudaMalloc((void**) &deviceOutSize, sizeof(int)), "Failed to allocate device memory");
-    cudaCheckError(cudaMalloc((void**) &deviceFlagSize, sizeof(int)), "Failed to allocate device memory");
+    cudaCheckError(cudaMalloc((void**) &deviceInBuf, inSize), "Failed to allocate deviceInBuf");
+    cudaCheckError(cudaMalloc((void**) &deviceOutBuf, outSize), "Failed to allocate deviceOutBuf");
+    cudaCheckError(cudaMalloc((void**) &deviceOutSize, sizeof(int)), "Failed to allocate deviceOutSize");
+    cudaCheckError(cudaMalloc((void**) &deviceFlagSize, sizeof(int)), "Failed to allocate deviceFlagSize");
 
     printf("Copying data from host to device...\n");
-    cudaCheckError(cudaMemcpy(deviceInBuf, inBuf, inSize, cudaMemcpyHostToDevice), "Failed to copy memory to device");
-    cudaCheckError(cudaMemset(deviceFlagOut, 0, sizeof(CompressFlagBlock) * nFlagBlocks),
-        "Failed to set device memory");
+    cudaCheckError(cudaMemcpy(deviceInBuf, inBuf, inSize, cudaMemcpyHostToDevice), 
+        "Failed to copy deviceInBuf to device");
+    cudaCheckError(cudaMemset(deviceFlagOut, 0, sizeof(CompressFlagBlock) * nFlagBlocks), 
+        "Failed to set deviceFlagOut to 0");
 
     timer.begin();
     //CompressKernel<<<,>>>();
@@ -54,13 +55,14 @@ std::pair<bool, double> CUDALZSS::compress(const uint8_t* inBuf, int inSize,
 
     printf("Copying data from device to host...\n");
     cudaCheckError(cudaMemcpy(&outSize, deviceOutSize, sizeof(int), cudaMemcpyDeviceToHost),
-        "Failed to copy memory to host");
+        "Failed to copy deviceOutSize to host");
     cudaCheckError(cudaMemcpy(&flagSize, deviceFlagSize, sizeof(int), cudaMemcpyDeviceToHost),
-        "Failed to copy memory to host");
+        "Failed to copy deviceFlagSize to host");
 
-    cudaCheckError(cudaMemcpy(outBuf, deviceOutBuf, outSize, cudaMemcpyDeviceToHost), "Failed to copy memory to host");
+    cudaCheckError(cudaMemcpy(outBuf, deviceOutBuf, outSize, cudaMemcpyDeviceToHost), 
+        "Failed to copy deviceOutBuf to host");
     cudaCheckError(cudaMemcpy(flagOut, deviceFlagOut, sizeof(CompressFlagBlock) * nFlagBlocks, cudaMemcpyDeviceToHost),
-        "Failed to copy memory to host");
+        "Failed to copy deviceFlagOut to host");
     
     return std::make_pair(true, elapsed);
 }
