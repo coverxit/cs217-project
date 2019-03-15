0441670f4609fc9ed0bf90fb27c393b4f76cca19
Shindo <cn.shindo@gmail.com>
2019-03-14 17:07:04 -0700

    More flex

--- Examples/test_example.sh
+++ Examples/test_example.sh
@@ -5,6 +5,8 @@ corpus=(canterbury_corpus large_corpus miscellaneous_corpus)
 logs=(cpu_st.comp.log cpu_mt.comp.log gpu.comp.log)
 flags=(s m g)
 modes=(CPU-ST CPU-MT GPU)
+tmp_comp=tmp.comp
+tmp_decomp=tmp.decomp
 
 if [ ! -f ${exec} ]; then
     echo "Please compile the program first!"
@@ -25,16 +27,16 @@ for d in ${corpus[@]}; do
 
             for i in "${!flags[@]}"; do
                 echo "  Running kernel ${modes[$i]}..."
-                rm -f tmp.comp
-                rm -f tmp.decomp
+                rm -f ${tmp_comp}
+                rm -f ${tmp_decomp}
 
-                ${exec} c${flags[$i]} ${f} tmp.comp >> ${logs[$i]}
+                ${exec} c${flags[$i]} ${f} ${tmp_comp} >> ${logs[$i]}
                 echo '----------------------------------------------------------------------------------' >> ${logs[$i]}
-                comp_checksum=$(sha1sum tmp.comp | cut -f1 -d' ')
+                comp_checksum=$(sha1sum ${tmp_comp} | cut -f1 -d' ')
                 echo "    Compression SHA1:   ${comp_checksum}"
 
-                ${exec} d${flags[$i]} tmp.comp tmp.decomp > /dev/null
-                decomp_checksum=$(sha1sum tmp.decomp | cut -f1 -d' ')
+                ${exec} d${flags[$i]} ${tmp_comp} ${tmp_decomp} > /dev/null
+                decomp_checksum=$(sha1sum ${tmp_decomp} | cut -f1 -d' ')
                 
                 if [ "${checksum}" == "${decomp_checksum}" ]; then
                     echo "    Decompression SHA1: ${decomp_checksum}, Match: Yes"
@@ -47,5 +49,5 @@ for d in ${corpus[@]}; do
     done
 done
 
-rm -f tmp.comp
-rm -f tmp.decomp
+rm -f ${tmp_comp}
+rm -f ${tmp_decomp}
